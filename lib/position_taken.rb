155958af1567f1202f10f95b65139b3c6ffc6696
Codi Kraus <codi.kraus@gmail.com>
2021-04-21 00:28:49 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
+  if board[index] != " "
+  elsif board[index] != ""
+  elsif board[index] != nil
+  else board[index] == "X" or "O"
 end
