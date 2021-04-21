d85885e4966bfcef7fd3875eab92f4b0d3087c4b
Codi Kraus <codi.kraus@gmail.com>
2021-04-21 00:47:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  check_position = board[index]
-  if check_position =! " " 
+  if board[index] == " "
+    return !true 
 end
